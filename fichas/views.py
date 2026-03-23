c13c697ede9599729b0be9763712428bf932a5f0
Roberto Otarola <roberto.otarola@proton.me>
2026-03-23 11:42:50 -0300

    feat: CRUD views using class-based views

--- fichas/views.py
+++ fichas/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import *
+from .models import Dueno, Mascota
 
-# Create your views here.
+
+# ===== DUEÑO =====
+class DuenoListView(ListView):
+    model = Dueno
+
+
+class DuenoDetailView(DetailView):
+    model = Dueno
+
+
+class DuenoCreateView(CreateView):
+    model = Dueno
+    fields = '__all__'
+    success_url = reverse_lazy('fichas:dueno_lista')
+
+
+class DuenoUpdateView(UpdateView):
+    model = Dueno
+    fields = '__all__'
+    success_url = reverse_lazy('fichas:dueno_lista')
+
+
+class DuenoDeleteView(DeleteView):
+    model = Dueno
+    success_url = reverse_lazy('fichas:dueno_lista')
+
+
+# ===== MASCOTA =====
+class MascotaListView(ListView):
+    model = Mascota
+
+
+class MascotaDetailView(DetailView):
+    model = Mascota
+
+
+class MascotaCreateView(CreateView):
+    model = Mascota
+    fields = '__all__'
+    success_url = reverse_lazy('fichas:mascota_lista')
+
+
+class MascotaUpdateView(UpdateView):
+    model = Mascota
+    fields = '__all__'
+    success_url = reverse_lazy('fichas:mascota_lista')
+
+
+class MascotaDeleteView(DeleteView):
+    model = Mascota
+    success_url = reverse_lazy('fichas:mascota_lista')
